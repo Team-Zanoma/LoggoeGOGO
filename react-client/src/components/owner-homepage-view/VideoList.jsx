0f5202c5336001dc9784b0426cc5e3926ee13884
Pedro Barquinha <barquinha.pedro@gmail.com>
2018-04-24 15:33:08 -0400

    Finished owner search, added a youtube search and add view + delete functionality.

--- react-client/src/components/owner-homepage-view/VideoList.jsx
+++ react-client/src/components/owner-homepage-view/VideoList.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import VideoListEntry from './VideoListEntry.jsx';
 import Paper from 'material-ui/Paper';
 
-const VideoList = ({videos, redirect}) => (
+const VideoList = ({videos, redirect, view, add, remove}) => {
+  return (
     <Paper style={style}>
       <div>
-        {videos.map((video, i) => <VideoListEntry key={i} video={video} redirect={redirect}/>)}
+        {videos.map((video, i) => <VideoListEntry key={i} video={video} redirect={redirect} view={view} add={add} remove={remove}/>)}
       </div>
     </Paper>
-)
+  )
+}
 
 const style = {
   height: 'auto',
