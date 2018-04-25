20192c886f3a0caeec706740cedb0f635464081a
mrMustachos <bill.domanick@crowdsurge.com>
2018-04-24 20:05:43 -0400

    we now have the ability to use react-router by fixing this horrible webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/react-client/src/');
+const webpack = require('webpack');
+const path = require('path');
 
-var DIST_DIR = path.join(__dirname, '/react-client/dist');
+const parentDir = path.join(__dirname, '/react-client/src/');
+const distDir = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-       }
-      },
-      {
-        test: /\.css?/,
-        loader: 'css-loader'
+  entry: [
+    path.join(parentDir, 'index.jsx')
+  ],
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },{
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"]
       }
     ]
+  },
+  output: {
+    path: distDir,
+    filename: 'bundle.js'
   }
 };
