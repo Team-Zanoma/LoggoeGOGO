2a8efe629573ce892c74f3c41d8f8d19d9466731
Pedro Barquinha <barquinha.pedro@gmail.com>
2018-04-25 12:28:28 -0400

    Pie chart view for tags implemented.

--- react-client/src/components/owner-video-view/Analytics.jsx
+++ react-client/src/components/owner-video-view/Analytics.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import c3 from 'c3';
 import 'c3/c3.css';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Analytics extends React.Component {
   constructor(props) {
@@ -9,10 +10,14 @@ class Analytics extends React.Component {
 
     this.state = { 
       buckets: [],
-      counts: []
+      counts: [],
+      view: 'chart',
+      tags: []
     };
+
     this.getBuckets = this.getBuckets.bind(this);
     this.createChart = this.createChart.bind(this);
+    this.chartGenCount = 0;
   }
 
   componentDidMount() {
@@ -31,32 +36,78 @@ class Analytics extends React.Component {
         buckets: data.data.map((row) => row.TimeStampGroup),
         counts: data.data.map((row) => row.total)
       })
+      this.arrangeTags();
     })
   }
 
-  createChart() {
-      const data = this.state.counts.slice();
+  arrangeTags() {
+    const tags = {};
 
+    this.props.timeStamps.forEach(comment => {
+      let tag = comment.tag;
+      if (tags.hasOwnProperty(tag)) {
+        tags[tag].push(1);
+      } else {
+        tags[tag] = [2];
+      }
+    })
+
+    const tagsArray = Object.entries(tags);
+    tagsArray.forEach((arr, i) => {
+      tagsArray[i] = [arr[0] ,...arr[1]]
+    })
+    this.setState({tags: tagsArray})
+  }
+
+  createChart(type) {
+    const data = this.state.counts.slice();
+    const times = this.state.buckets.slice();
+    times[times.length-1] = times[times.length-1] + '+';
+
+    if (type === 'chart') {
+      const id = this.chartGenCount === 1 ? '#chart' : '#pie';
       const chart = c3.generate({
+        bindto: id,
         data: {
           columns: [
-            ['data', ...data]
+            ['confused', ...data]
           ],
         },
         axis: {
             x: {
                 type: 'category',
-                categories: this.state.buckets
+                categories: times
             }
         }
-   });
+      });
+    } else {
+      console.log(this.state.tags)
+      const pie = c3.generate({
+          bindto: '#chart',
+          data: {
+              columns: this.state.tags,
+              type : 'pie',
+              onclick: function (d, i) { console.log("onclick", d, i); },
+              onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+              onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+          }
+      });
+    }
+    this.chartGenCount++;
   }
+
   
   render() {
-    this.createChart();
+    this.createChart(this.state.view);
     return (
-      <div id="chart">
-      </div>
+      <div>
+      <RaisedButton label={this.state.view === 'chart' ? 'pie' : 'chart'} 
+      onClick={() => {
+        this.setState({view: this.state.view === 'chart' ? 'pie' : 'chart'});
+      }}/>
+        <div id={this.state.view === 'chart' ? 'chart' : 'pie'}>
+        </div>
+      </div>  
     );
   }
 
