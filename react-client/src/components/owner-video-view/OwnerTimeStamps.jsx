3004c1ab87ea101c09eda225773557b704d5c81e
Andrew Lee <andrewylee@Andrews-MacBook-Air.local>
2018-04-25 17:45:53 -0400

    updated owner videos

--- react-client/src/components/owner-video-view/OwnerTimeStamps.jsx
+++ react-client/src/components/owner-video-view/OwnerTimeStamps.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Chip from 'material-ui/Chip';
 import Paper from 'material-ui/Paper';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
+import VideoPlayerComponent from '../VideoPlayerComponent.jsx';
 
 class OwnerTimeStamps extends React.Component {
   constructor(props) {
@@ -9,18 +10,24 @@ class OwnerTimeStamps extends React.Component {
   }
 
   render() {
-
     // create the list of timestamp items, then render the list
     return (
       <div>
         <h2>Timestamps</h2>
         {this.props.timeStamps.map((timeStamp, i) => (
           <Card style={{margin: '20px'}}>
+            
             <CardHeader 
             style={{backgroundColor: 'grey'}}
             title={'Time: ' + (timeStamp.timestamp / 60 | 0) + ':' + String(timeStamp.timestamp % 60).padStart(2, '0')} 
             subtitle={'name: ' + timeStamp.name}/> 
             <CardText>
+    
+              <VideoPlayerComponent 
+                videoId={this.props.videoId} 
+                startingTimestamp={timeStamp.timestamp}
+              />
+     
               <b>Comment:</b>&nbsp;{timeStamp.comment}
               <br/>
               <strong>Tag:</strong><Chip style={{display: 'inline', backgroundColor: '#999999', margin: '0 0 0 5px', color: 'white'}}>{timeStamp.tag ? timeStamp.tag : 'n/a'}</Chip>
