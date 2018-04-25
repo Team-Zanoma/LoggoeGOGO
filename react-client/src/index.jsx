0446b4f44f1f4995fdc31b946ce65e9587334ef9
mrMustachos <bill.domanick@crowdsurge.com>
2018-04-25 13:47:45 -0400

    fixing the index.jsx

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,6 @@
-// import { BrowserRouter as Router, Route, Link, Switch, History } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect, withRouter } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch, History} from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import React, { Component } from 'react';
+import React from 'react';
 
 import RegistrationPage from './components/RegistrationPageView.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -11,46 +10,23 @@ import StudentVideo from './components/StudentVideoView.jsx';
 import LandingPage from './components/LandingPageView.jsx';
 import OwnerVideo from './components/OwnerVideoView.jsx';
 
-// import AuthExample from './AuthExample.js';
-import fakeAuth from './utils/fakeAuth.js';
-import './App.css';
-
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      redirectToReferrer: false
-    }
   }
 
-  login = () => {
-    fakeAuth.authenticate(() => {
-      this.setState({ redirectToReferrer: true });
-    });
-  };
-
   render () {
-
-    const PrivateRoute = ({ component: Component, ...rest }) => (
-      <Route {...rest} render={
-        props => fakeAuth.isAuthenticated
-          ? (<Component {...props} />)
-          : (<Redirect to={{ pathname: "/", state: { from: props.location } }} /> )
-        }
-      />
-    );
-
     return (
       <MuiThemeProvider>
         <Router>
-          <div>
-            <Route path='/' render={(props) => (<LandingPage { ...props } data={ this.login }/>)}/>
-            <Route exact path="/registration" component={ RegistrationPage }/>
-            <PrivateRoute exact path="/student" component={ StudentHomepage }/>
-            <PrivateRoute exact path="/student/video" component={ StudentVideo }/>
-            <PrivateRoute exact path="/owner" component={ OwnerHomepage }/>
-            <PrivateRoute exact path="/owner/video" component={ OwnerVideo }/>
-          </div>
+          <Switch>
+            <LandingPage exact path="/" component={LandingPage}/>
+            <RegistrationPage exact path="/registration" component={RegistrationPage}/>
+            <StudentHomepage exact path="/student" component={StudentHomepage}/>
+            <StudentVideo exact path="/student/video" component={StudentVideo}/>
+            <OwnerHomepage exact path="/owner" component={OwnerHomepage}/>
+            <OwnerVideo exact path="/owner/video" component={OwnerVideo}/>
+          </Switch>
         </Router>
       </MuiThemeProvider>
     )
@@ -58,20 +34,3 @@ class App extends Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-// <AuthExample />
-
-// <Route exact path="/" component={ LandingPage }/>
-
-      // <MuiThemeProvider>
-      //   <Router>
-      //     <div>
-      //       <Route path='/' render={(props) => (<LandingPage { ...props } data={ this.login }/>)}/>
-      //       <Route exact path="/registration" component={ RegistrationPage }/>
-      //       <PrivateRoute exact path="/student" component={ StudentHomepage }/>
-      //       <PrivateRoute exact path="/student/video" component={ StudentVideo }/>
-      //       <PrivateRoute exact path="/owner" component={ OwnerHomepage }/>
-      //       <PrivateRoute exact path="/owner/video" component={ OwnerVideo }/>
-      //     </div>
-      //   </Router>
-      // </MuiThemeProvider>
