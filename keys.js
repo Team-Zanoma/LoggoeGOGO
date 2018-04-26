4c502b9ce2c47f166a0e8e02837981d08dbdcd87
Pedro Barquinha <34924804+pedrobarquinha@users.noreply.github.com>
2018-04-26 10:41:32 -0400

    Delete keys.js

--- keys.js
+++
@@ -1,7 +0,0 @@
-const keys = require('./config.js');
-module.exports = {
-  host     : process.env.host || keys.host,
-  user     : process.env.user || keys.user,
-  database : process.env.database || keys.database,
-  password: process.env.password || keys.password
-}
