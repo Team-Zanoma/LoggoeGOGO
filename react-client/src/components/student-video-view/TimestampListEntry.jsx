ecd8a8e371272c17386ae5a7794fb987208a077e
mrMustachos <bill.domanick@crowdsurge.com>
2018-04-27 15:41:19 -0400

    Timestamps styled

--- react-client/src/components/student-video-view/TimestampListEntry.jsx
+++ react-client/src/components/student-video-view/TimestampListEntry.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import moment from 'moment';
 
-class TimestampListEntry extends React.Component {
+import './TimestampListEntry.css';
+
+class TimestampListEntry extends Component {
   constructor(props) {
     super(props)
     
@@ -18,36 +22,83 @@ class TimestampListEntry extends React.Component {
   onDeleteTimestamp() {
     this.props.deleteTimestamp(this.props.timestamp.timestamp)
   }
+
+  formatTimestamp = (num) => `${(num / 60 | 0)}:${String(num % 60).padStart(2, '0')}`;
+
+  // showCardActions = () => {
+  //   return (
+  //     view === 'timestamps'
+  //     ? (
+  //         <CardActions>
+  //           <FlatButton onClick={ this.onChangeVideo } label="Watch This Clip" />
+  //           <FlatButton onClick={ this.onDeleteTimestamp } label="Delete Question" />
+  //         </CardActions>
+  //       )
+  //     : ( <span><i>{moment().fromNow()}</i></span> )
+  //   );
+  // }
   
   render() {
-    const {timestamp, view} = this.props;
+    const { timestamp, view, tag } = this.props;
     return (
-      <Paper style={style}>
-        <div>
-          <h4 style={{display: 'inline'}}>{ view === 'timestamps' ? 'Timestamp' : timestamp.username + ':'} </h4> 
-          { view === 'timestamps' ? (timestamp.timestamp / 60 | 0) + ':' + String(timestamp.timestamp % 60).padStart(2, '0')
-            : ''
-          }
-        </div>
-        <div>
-          <h4 style={{display: 'inline'}}>{ view === 'timestamps' ? 'Comment:' : ''} </h4> 
-          {view === 'timestamps' ? timestamp.comment : timestamp.message }
-        </div>
-        <span><i>{moment().fromNow()}</i></span>
-        <div>
-          {view === 'timestamps' ? <button onClick={this.onChangeVideo}>Watch This Clip</button> : ''}
-          {view === 'timestamps' ? <button onClick={this.onDeleteTimestamp}>X</button> : ''}
-        </div>
-      </Paper>
-      );
+      <Card style={ style.card }>
+        <CardHeader
+          title="Issue"
+          subtitle={ this.formatTimestamp(timestamp.timestamp) }
+          subtitleStyle={ style.sub }
+          titleStyle={ style.main }
+          className='fixer'
+        />
+        <CardText>
+          { timestamp.comment }
+        </CardText>
+        <CardActions>
+          <FlatButton onClick={ this.onChangeVideo } label="Watch" />
+          <FlatButton onClick={ this.onDeleteTimestamp } label="Delete" />
+        </CardActions>
+      </Card>
+    );
   }
 }
 
 const style = {
-  width: '80%', 
-  margin: '10px', 
-  padding: '20px', 
-  float: 'left'
+  main: {
+    display: 'inline-block',
+    float: 'left',
+    lineHeight: '21px'
+  },
+  sub: {
+    display: 'inline-block',
+    float: 'right',
+    lineHeight: '21px'
+  },
+  card: {
+    marginBottom: '10px'
+  }
 }
 
 export default TimestampListEntry;
+
+
+// const style = {
+//   width: '100%', 
+//   padding: '20px', 
+//   float: 'left'
+// }
+      // <Paper style={ style }>
+      //   <div>
+      //     <h4 style={{display: 'inline'}}>{ view === 'timestamps' ? 'Timestamp' : timestamp.username + ':'} </h4> 
+      //     { view === 'timestamps' ? (timestamp.timestamp / 60 | 0) + ':' + String(timestamp.timestamp % 60).padStart(2, '0')
+      //       : ''
+      //     }
+      //   </div>
+      //   <div>
+      //     <h4 style={{display: 'inline'}}>{ view === 'timestamps' ? 'Comment:' : ''} </h4> 
+      //     {view === 'timestamps' ? timestamp.comment : timestamp.message }
+      //   </div>
+      //   <span><i>{moment().fromNow()}</i></span>
+      //   <div>
+      //     {view === 'timestamps' ? <button onClick={ this.onChangeVideo }>Watch This Clip</button> : ''}
+      //     {view === 'timestamps' ? <button onClick={ this.onDeleteTimestamp }>X</button> : ''}
+      //   </div>
+      // </Paper>
